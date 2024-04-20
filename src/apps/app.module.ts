6a0cfda74bb355086ce378681bd8a8075623f73a
Elyes5 <elyes.saddem@etudiant-isi.utm.tn>
2024-04-21 01:43:13 +0200

    PROJECT_STRUCTURE

--- src/apps/app.module.ts
+++ src/apps/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material/lib/material.module';
+import { NavbarComponent } from './libs/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
