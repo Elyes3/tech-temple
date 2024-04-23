28dd06e5465f1a518f959588b0026605585ec379
ahmed <ahmed.welhazir8@gmail.com>
2024-04-23 18:18:59 +0100

    merge conflict resolve

--- src/app/libs/products/products.module.ts
+++ src/app/libs/products/products.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product_detailsComponent } from './components/product_details/product_details.component';
-import { MaterialModule } from 'src/apps/material/lib/material.module';
+import { MaterialModule } from 'src/app/material/lib/material.module';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { MatTabsModule } from '@angular/material/tabs';
 
