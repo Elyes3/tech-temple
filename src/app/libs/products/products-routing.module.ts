ff9766520f2799cdac33f8543fa4db6dae3412eb
ahmed <ahmed.welhazir8@gmail.com>
2024-05-01 02:10:39 +0100

    home page v1.0.0

--- src/app/libs/products/products-routing.module.ts
+++ src/app/libs/products/products-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Product_detailsComponent } from './components/product_details/product_details.component';
+import { Home_pageComponent } from './components/home_page/home_page.component';
 const routes: Routes = [
   { path: 'details', component: Product_detailsComponent },
+  { path: 'home', component: Home_pageComponent },
 ];
 
 @NgModule({
