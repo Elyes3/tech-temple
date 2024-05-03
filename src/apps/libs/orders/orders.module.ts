64628e923506ddcb9678c76cd69c96ffb54d26c2
rami9237 <rami.benromdhane@etudiant-isi.utm.tn>
2024-05-03 19:25:25 +0100

    update

--- src/apps/libs/orders/orders.module.ts
+++ src/apps/libs/orders/orders.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MaterialModule } from 'src/apps/material/lib/material.module';
+import { BasketComponent } from './components/basket/basket.component';
 
 
 @NgModule({
   declarations: [
+    BasketComponent
   ],
   imports: [
+    MaterialModule,
     CommonModule
   ]
 })
--- src/apps/material/lib/material.module.ts
+++ src/apps/material/lib/material.module.ts
@@ -3,18 +3,22 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatStepperModule} from '@angular/material/stepper';
+import {MatCardModule} from '@angular/material/card';
+
 @NgModule({
   imports: [
     MatInputModule,
     MatButtonModule,
     MatFormFieldModule,
     MatStepperModule,
+    MatCardModule
   ],
   exports: [
     MatInputModule,
     MatButtonModule,
     MatFormFieldModule,
-    MatStepperModule
+    MatStepperModule,
+    MatCardModule
   ]
 })
 export class MaterialModule {}
