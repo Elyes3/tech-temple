05874f3de2f600450249bbbb38fb175066842da0
rami9237 <rami.benromdhane@etudiant-isi.utm.tn>
2024-05-09 15:23:13 +0100

    add cart service and init cart page

---
+++ src/app/libs/orders/components/basket-item/basket-item.component.ts
@@ -0,0 +1,57 @@
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { CartItem } from 'src/app/libs/shared/models/CartItem';
+import { CartService } from '../../services/cart.service';
+
+@Component({
+  selector: 'app-basket-item',
+  templateUrl: './basket-item.component.html',
+  styleUrls: ['./basket-item.component.scss']
+})
+export class BasketItemComponent implements OnInit, OnDestroy {
+  @Input() item!: CartItem;
+  totalPrice!: number;
+  private cartItemsSubscription!: Subscription;
+
+  constructor(private cartService: CartService) { }
+
+  ngOnInit() {
+    this.calculateTotalPrice();
+    this.cartItemsSubscription = this.cartService.cartItems$.subscribe(cartItems => {
+      const updatedItem = cartItems.find(cartItem => cartItem.id === this.item.id);
+      if (updatedItem) {
+        this.item = updatedItem;
+        this.calculateTotalPrice();
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.cartItemsSubscription.unsubscribe();
+  }
+
+  incrementQuantity() {
+    this.item.quantity++;
+    this.updateItem();
+  }
+
+  decrementQuantity() {
+    if (this.item.quantity > 0) {
+      this.item.quantity--;
+      this.updateItem();
+    }
+  }
+
+  updateItem() {
+    this.calculateTotalPrice();
+    this.cartService.updateCartItem(this.item);
+  }
+
+  calculateTotalPrice() {
+    this.totalPrice = this.item.product.price * this.item.quantity;
+  }
+
+  onDeleteButtonClick() {
+    this.cartService.removeItemFromCart(this.item.id);
+  }
+}
--- src/app/libs/orders/orders.module.ts
+++ src/app/libs/orders/orders.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BasketItemComponent } from './components/basket-item/basket-item.component';
+import { BasketComponent } from './components/basket/basket.component';
+import { CartService } from './services/cart.service';
 
 
 
 @NgModule({
   declarations: [
+    BasketItemComponent,
+    BasketComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    BasketItemComponent
+  ],
+  providers: [
+    CartService
   ]
 })
 export class OrdersModule { }
