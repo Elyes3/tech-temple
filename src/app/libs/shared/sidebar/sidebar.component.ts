76af552eb776912af333a8bcb13094c0834b975a
Yassin Ben Chaaben <122620181+BCHYassin@users.noreply.github.com>
2024-05-12 16:15:28 +0100

    modified sidebar category fetching

--- src/app/libs/shared/sidebar/sidebar.component.ts
+++ src/app/libs/shared/sidebar/sidebar.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ComponentVisibilityService } from '../service/ComponentVisibilityService';
+import { CategoriesService } from '../../dashboard/services/categories.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
 
   categories = [
     'Computers',
@@ -16,8 +17,22 @@ export class SidebarComponent {
     'Smart Home',
   ]
 
+  newCategories: any
+
   isVisible$ = this.componentVisibilityService.isVisible$;
 
-  constructor(private componentVisibilityService: ComponentVisibilityService) {}
+  constructor(private componentVisibilityService: ComponentVisibilityService, private categoriesService: CategoriesService) {}
+
+  ngOnInit(): void {
+    this.categoriesService.loadCategories().subscribe(
+      (data: any) => { 
+        console.log(data)
+        this.newCategories = data;
+      },
+      (error: any) => {
+        console.error('An error occurred:', error);
+      },
+    )
+  }
 
 }
