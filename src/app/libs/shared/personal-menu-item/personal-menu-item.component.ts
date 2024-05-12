66c1ed59c8118d28b2db1b7351be9365d0d522cf
rami9237 <rami.benromdhane@etudiant-isi.utm.tn>
2024-05-12 19:52:08 +0100

    addding logout removing notifications

--- src/app/libs/shared/personal-menu-item/personal-menu-item.component.ts
+++ src/app/libs/shared/personal-menu-item/personal-menu-item.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { UsersFacade } from '../../state/users/users.facade';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-personal-menu-item',
@@ -7,7 +9,16 @@ import { UsersFacade } from '../../state/users/users.facade';
   styleUrls: ['./personal-menu-item.component.scss']
 })
 export class PersonalMenuItemComponent {
-  constructor(private usersFacade: UsersFacade) { }
+  constructor(private usersFacade: UsersFacade, private router: Router) { }
   authenticatedUser$ = this.usersFacade.authenticatedUser$;
-  
+  subscription!: Subscription;
+  logout() {
+    this.usersFacade.logoutAuthenticatedUser();
+    this.subscription = this.usersFacade.authenticatedUser$.subscribe(authenticatedUser => {
+      if (authenticatedUser.authenticatedUser == null) {
+        console.log(authenticatedUser.authenticatedUser);
+        this.router.navigateByUrl('/', { state: { logout: true } })
+      }
+    })
+  }
 }
